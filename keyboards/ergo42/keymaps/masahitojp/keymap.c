057812cee3c81e917cf88c679190f2f4a8cc2cc1
masahitojp <randomstep@gmail.com>
2019-01-01 20:25:48 +0900

    add my ergo42 keymap

--- keyboards/ergo42/keymaps/masahitojp/keymap.c
+++ keyboards/ergo42/keymaps/masahitojp/keymap.c
@@ -21,14 +21,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
    * | SYMB | LSft |   Z  |   X  |   C  |   V  |  B   |   |  N   |   M  |   ,  |   .  |   /  |  UP  | RSft |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | LGUI | TAB  |      |      | LAlt | Bksp |Space |   |Enter | RAlt |   -  |   =  | LEFT | DOWN | RIGHT|
+   * | LGUI | TAB  |      |      | LAlt | RAlt | Bksp |   |Space |Enter |   -  |   =  | LEFT | DOWN | RIGHT|
    * `------------------------------------------------'   `------------------------------------------------'
    */
     [BASE] = LAYOUT(
         KC_ESC, KC_GRV, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC,
         MO(META), KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_BSLS,
         MO(SYMB), KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_UP, KC_RSFT,
-        KC_LGUI, KC_TAB, XXXXXXX, XXXXXXX, KC_LALT, KC_BSPC, KC_SPC, KC_ENT, KC_RALT, KC_MINS, KC_EQL, KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LGUI, KC_TAB, XXXXXXX, XXXXXXX, KC_LALT, KC_RALT, KC_BSPC, KC_SPC, KC_ENT, KC_MINS, KC_EQL, KC_LEFT, KC_DOWN, KC_RGHT),
 
     /* META
    * ,------------------------------------------------.   ,------------------------------------------------.
